96891c506909d5ad57391f7e1226d179d499adb8
keita miyano <keita.miyano@estie.co.jp>
2020-01-04 18:06:00 +0900

    add README

---
+++ section14/README.md
@@ -0,0 +1,12 @@
+# Clone
+
+```
+cd $GOPATH/src # or ~/go/src
+git clone git@github.com:mynkit/go_lesson.git
+```
+
+# Requires
+
+```sh
+go get gopkg.in/ini.v1
+```
--- section14/main.go
+++ section14/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go_lesson/section14/config"
+)
 
 func main() {
 	fmt.Println("vim-go")
+	fmt.Println(config.Config.ApiKey)
+	fmt.Println(config.Config.ApiSecret)
 }
